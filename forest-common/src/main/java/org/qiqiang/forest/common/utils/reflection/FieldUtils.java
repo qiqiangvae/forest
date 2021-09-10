897bfe6b2b99816a062d29ba2ecb7ad966ab5c0e
qiqiang <qiqiangvae@foxmail.com>
2021-09-10 14:04:03 +0800

    反射工具类新增若干方法

--- forest-common/src/main/java/org/qiqiang/forest/common/utils/reflection/FieldUtils.java
+++ forest-common/src/main/java/org/qiqiang/forest/common/utils/reflection/FieldUtils.java
@@ -1,5 +1,7 @@
 package org.qiqiang.forest.common.utils.reflection;
 
+import org.qiqiang.forest.common.exception.ReflectForestException;
+
 import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -34,5 +36,25 @@ public class FieldUtils {
      */
     public static Set<Field> getFields(Class<?> clazz) {
         return Stream.of(clazz.getDeclaredFields()).collect(Collectors.toSet());
+    }
+
+    /**
+     * 获取指定字段
+     *
+     * @param clazz     Class
+     * @param fieldName field name
+     * @return field
+     */
+    public static Field getField(Class<?> clazz, String fieldName) {
+        Field field = null;
+        try {
+            field = clazz.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            Class<?> superclass = clazz.getSuperclass();
+            if (superclass != null) {
+                field = getField(superclass, fieldName);
+            }
+        }
+        return field;
     }
 }
--- forest-common/src/main/java/org/qiqiang/forest/common/utils/reflection/PropertyUtils.java
+++ forest-common/src/main/java/org/qiqiang/forest/common/utils/reflection/PropertyUtils.java
@@ -5,15 +5,38 @@ import org.qiqiang.forest.common.exception.ReflectForestException;
 import java.lang.reflect.Field;
 
 /**
+ * 类属性工具
+ *
  * @author qiqiang
  */
 public class PropertyUtils {
-    public static Object getValue(Field field, Object object) {
+    /**
+     * 获取一个对象的字段值
+     *
+     * @param field  字段
+     * @param object 对象
+     * @param <T>    target class
+     * @return field value
+     */
+    public static <T> T getValue(Field field, Object object) {
         field.setAccessible(true);
         try {
-            return field.get(object);
+            return (T) field.get(object);
         } catch (IllegalAccessException e) {
             throw new ReflectForestException(e);
         }
+    }
+
+    /**
+     * 获取一个对象的字段值
+     *
+     * @param fieldName 字段名
+     * @param object    对象
+     * @param <T>       target class
+     * @return field value
+     */
+    public static <T> T getValue(String fieldName, Object object) {
+        Field field = FieldUtils.getField(object.getClass(), fieldName);
+        return getValue(field, object);
     }
 }
